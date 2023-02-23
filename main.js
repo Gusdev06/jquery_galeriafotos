546ce5e4cdca02e8e8098dac3f8946bcd555bf2d
Gustavo gomes <coven688@exemplo.com>
2023-02-23 11:23:35 -0300

    firts commits

---
+++ main.js
@@ -0,0 +1,31 @@
+// o Jquery funciona a partir do $ e dos () e nos parenteses informamos o seletor que é o document e o .ready sigifnica que quando estiver pronto ele executa o alert
+
+$(document).ready(function() {
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const endereçoDaNovaImagem = $('#endereço-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${endereçoDaNovaImagem}" />`).appendTo(novoItem)
+        $(`
+            <div class="overlay-img-link">
+                <a href="${endereçoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem)
+        $(novoItem).appendTo('ul')
+        $(novoItem).fadeIn(1000);
+        $('#endereço-imagem-nova').val('')
+    })
+})
+
+
+    
